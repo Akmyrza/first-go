b5b48f926322e11291ee772ad481f8d9e03668d6
Akmyrza <onlyfordox@mail.ru>
2022-06-21 18:00:35 +0600

    changed

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
-)	
+)
 
 func main() {
 	name := os.Getenv("USERNAME")
-	fmt.Printf("Well done %s for having your first go\n", name)
 	fmt.Printf("Well done %s for having your first go\n", name)
 }
